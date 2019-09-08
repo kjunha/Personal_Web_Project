44e7812d60d9734189e2c29a1b3acf6573726d10
JunhaKim <kjunha77@gmail.com>
2019-09-08 00:55:48 -1000

    add cs401 php mode

--- html/cs401/intro.php
+++ html/cs401/intro.php
@@ -26,19 +26,17 @@
           </div>
         </div>
         <div class="fav_links">
-          <!-- TODO: Icon -->
-          <span><i class="fab fa-github"></i> Github</span>
-          <span><i class="fab fa-linkedin"></i> Linkedin</span>
-          <span><i class="fab fa-facebook"></i> Facebook</span>
-          <span><i class="fab fa-instagram"></i> Instagram</span>
-          <span><i class="fas fa-at"></i> Sand Email</span>
+          <span onclick="goGithub()"><i class="fab fa-github"></i> Github</span>
+          <span onclick="goLinkedin()"><i class="fab fa-linkedin"></i> Linkedin</span>
+          <span onclick="goFacebook()"><i class="fab fa-facebook"></i> Facebook</span>
+          <span onclick="goInstagram()"><i class="fab fa-instagram"></i> Instagram</span>
+          <span onclick="goContactMe()"><i class="fas fa-at"></i> Sand Email</span>
         </div>
       </div>
 
       <!-- Part2: Contents -->
       <div class="contents">
         <div class="introduction">
-          <!-- TODO: Icon -->
           <span id="sectionTitle"><i class="fas fa-globe-asia"></i> Introduction</span>
           <div class="summary">
             <p>Brigham Young University - Hawaii</p>
@@ -66,29 +64,48 @@
           <div class="console">
             <!-- php part here -->
             <div id="profile45"><i class="far fa-handshake"></i></div>
-            <span>Welcome Message</span>
+            <span id="postTitle">Welcome Message</span>
             <div id="contentsBox">
-              
+              <?php
+                  if($_GET['myname'] === null):
+                    echo "<p>You can get a welcome message.</p>";
+                    echo "<p>Please follow Readme.md post.</p>";
+                  else:
+                    date_default_timezone_set('Pacific/Honolulu');
+                    echo "<p id='success'>Welcome ".$_GET['myname']."!</p>";
+                    echo "<p id='success'>You visited this website on..</p>";
+                    echo "<p id='success'>".date("M/d/Y")." (".date("D").") ".date("h:i:s a")." HST.</p>";
+                  endif;
+              ?>
             </div>
           </div>
           <div class="readme">
             <div id="profile45"><i class="fab fa-readme"></i></div>
-            <span>Readme.md</span>
+            <span id="postTitle">Readme.md</span>
             <div id="contentsBox">
-
+              <p>With the current URL 'is3.byuh.edu/html/intro.php', add following</p>
+              <p>?myname=yourname</p>
+              <p>Or, copy and paste this address to your url bar and change 'yourname' to your name.</p>
+              <a>is3.byuh.edu/html/intro.php?myname=yourname</a>
             </div>
           </div>
           <div class="sourcecode">
             <div id="profile45"><i class="fas fa-code"></i></div>
-            <span>Source Code</span>
+            <span id="postTitle">Source Code</span>
             <div id="contentsBox">
+              <p>Want to see the source code of this project?</p>
+              <p>Visit this Github repository</p>
+              <a href="https://github.com/kjunha/Personal_Web_Project/tree/master/html/cs401">https://github.com/kjunha/Personal_Web_Project/tree/master/html/cs401</a>
 
             </div>
           </div>
           <div class="gallery">
             <div id="profile45"><i class="far fa-images"></i></div>
-            <span>My Gallery</span>
+            <span id="postTitle">My Gallery</span>
             <div id="contentsBox">
+              <p>Cover background photo credit: Junha Dexter Kim.</p>
+              <p>Visit Instagram gallery to see more picture of the photographer</p>
+              <a href="https://www.instagram.com/junha_d._kim/">https://www.instagram.com/junha_d._kim/</a>
 
             </div>
           </div>
@@ -103,5 +120,24 @@
       $("#footer").load("../../footer.html");
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.js"></script>
+
+    <!-- Redirection Function -->
+    <script>
+      function goGithub(){
+        location.replace("https://github.com/kjunha")
+      }
+      function goLinkedin(){
+        location.replace("https://www.linkedin.com/in/junhadkim/")
+      }
+      function goFacebook(){
+        location.replace("https://www.facebook.com/kjunha77")
+      }
+      function goInstagram(){
+        location.replace("https://www.instagram.com/junha_d._kim/")
+      }
+      function goContactMe(){
+        location.replace("https://github.com/kjunha")
+      }
+    </script>
   </body>
 </html>
